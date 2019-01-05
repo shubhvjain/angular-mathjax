bbe8375e190f0f4c7a39242c3781838562a86f9b
Shubh V Jain <shubhvjain@gmail.com>
2019-01-05 22:09:53 +0530

    mathjax component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MathjaxComponent } from './mathjax/mathjax.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MathjaxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mathjax/mathjax.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { SimpleChanges } from '@angular/core';
+import { ConfigService } from '../config.service'
+@Component({
+  selector: 'mathjax',
+  templateUrl: './mathjax.component.html',
+  styleUrls: ['./mathjax.component.css']
+})
+export class MathjaxComponent implements OnChanges,OnInit {
+  @Input() content :string;
+
+  constructor(public cs: ConfigService) { }
+  mathJaxObject;
+
+  ngOnChanges(changes: SimpleChanges) {
+    // to render math equations again on content change
+    if (changes['content']) {
+      this.renderMath()
+    }
+  }
+  ngOnInit() {
+   this.loadMathConfig()
+   this.renderMath();
+  }
+
+  updateMathObt(){
+    this.mathJaxObject = this.cs.nativeGlobal()['MathJax'];
+  }
+
+  renderMath() {
+    this.updateMathObt();
+    let angObj = this;
+    setTimeout(() => {
+      angObj.mathJaxObject['Hub'].Queue(["Typeset", angObj.mathJaxObject.Hub], 'mathContent');
+    }, 1000)
+  }
+  loadMathConfig() {
+    this.updateMathObt();
+    this.mathJaxObject.Hub.Config({
+      showMathMenu: false,
+      tex2jax: { inlineMath: [["\(", "\)"]],displayMath:[["\[", "\]"]] },
+      menuSettings: { zoom: "Double-Click", zscale: "150%" },
+      CommonHTML: { linebreaks: { automatic: true } },
+      "HTML-CSS": { linebreaks: { automatic: true } },
+      SVG: { linebreaks: { automatic: true } }
+    });
+  }
+}
